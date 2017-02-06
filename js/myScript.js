e8e1bd393631d760e88a84afbc22c92bdf0e8ab8
Isa Rentacs <Isa-rentacs@users.noreply.github.com>
2017-02-06 23:33:55 +0900

    Added resize handler

--- js/myScript.js
+++ js/myScript.js
@@ -3,3 +3,14 @@ path.strokeColor = 'black';
 var start = new Point(100,100);
 path.moveTo(start);
 path.lineTo(start + [100,-50]);
+
+var circle = new Path.circle({
+    center: view.center,
+    radius:30,
+    strokeColor: 'black'
+});
+
+function onResize(event)
+{
+    path.position = view.center;
+}
